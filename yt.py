a75cf42ff15dae09183a4d3c3e13292a7442641e
Jacksone2307 <Jacksone2307@gmail.com>
2024-11-25 17:12:42 +1300

    Added GUI

--- yt.py
+++ yt.py
@@ -1,23 +1,16 @@
-import os
 import requests
-import google_auth_httplib2
-import googleapiclient
-import googleapiclient.discovery
-import googleapiclient.errors
-import google_auth_oauthlib.flow
-import json
 from googlesearch import search
 import re
 import webbrowser
 import multiprocessing
+from tkinter import *
 
-API_KEY = "AIzaSyBkOjW6lPXQ23EbK-5NJ3NuiDUaSDFuemE"
 
+"""
 
-scopes = ["https://www.googleapis.com/auth/youtube",
-          "https://www.googleapis.com/auth/youtube.force-ssl",
-          "https://www.googleapis.com/auth/youtube.readonly",
-          "https://www.googleapis.com/auth/youtubepartner"]
+USE JINJA/FLASK INSTEAD OF TKINTER, MORE FUNCTIONAL
+
+"""
 
 
 def remove_html_tags(text):
@@ -25,17 +18,6 @@ def remove_html_tags(text):
 
     clean = re.compile('<.*?>')
     return re.sub(clean, '', text)
-
-# class DataLoader:
-#     def __init__(self, api_url, api_key):
-#         self.api_url = api_url
-#         self.api_key = api_key
-
-#     def load_data(self, params):
-#         # Make API call using self.api_url and self.api_key
-#         # Return the loaded data
-#         response = requests.get(self.api_url, params=params)
-#         return response.json()
     
 
 
@@ -52,29 +34,35 @@ def search_and_display_key(qry):
             r = requests.get(i)
             text = remove_html_tags(str(r.content))
             key = re.search('with a ([A-G]) key and a (.*) mode', text)
-            print(key.group(1), key.group(2))
+            return (key.group(1), key.group(2))
+
+def induce_search(qry):
+    yt_query = qry + " youtube"
+    p1 = multiprocessing.Process(target=search_and_open_video, args=(yt_query, ))
+    
+    google_query = qry + " songbpm key"
+    p2 = multiprocessing.Process(target=search_and_display_key, args=(google_query, ))
+    p1.start()
+    p2.start()
+    key = p2.join()
+    p1.join()
+    return key
 
 def main():
-    qry_input = str(input("Search for: "))
-    while qry_input != "":
-        yt_query = qry_input + " youtube"
-        p1 = multiprocessing.Process(target=search_and_open_video, args=(yt_query, ))
-        
-        google_query = qry_input + " songbpm key"
-        p2 = multiprocessing.Process(target=search_and_display_key, args=(google_query, ))
-        p1.start()
-        p2.start()
-        p2.join()
-        p1.join()
-        qry_input = str(input("Search for: "))
-        
 
+    r = Tk()
+    r.title("SKF")
 
-    
+    qryLabel = Label(r, text="Song Name")
+    qryLabel.grid(row=0, column=0)
 
+    qryEntry = Entry(r)
+    qryEntry.grid(row=0,column=1)
 
-    
+    searchButton = Button(r, text="Search", command = lambda: induce_search(qryEntry.get()))
+    searchButton.grid(row=1, column=0, columnspan=2)
 
+    r.mainloop()
 
 if __name__ == "__main__":
     main()
