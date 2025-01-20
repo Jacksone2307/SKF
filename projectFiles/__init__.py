f5a36c00ca37c713cce988d5677c00417b5dd42b
Jacksone2307 <Jacksone2307@gmail.com>
2025-01-20 14:44:13 +1300

    Modified for spotify API - Deprecated

--- projectFiles/__init__.py
+++ projectFiles/__init__.py
@@ -4,14 +4,35 @@ from dotenv import find_dotenv, load_dotenv
 from urllib.parse import quote_plus, urlencode
 from flask import Flask, request, session, render_template, redirect, url_for
 from pathlib import Path
+from requests import post
+import base64
+import json
 
 """Initialise our Flask application"""
 
 
 oauth = None
 
-def instantiate_app():
+def get_token(client_id, client_secret):
+    """Obtain an access token from Spotify."""
 
+    auth_string = client_id + ':' + client_secret
+    auth_bytes = auth_string.encode('utf-8')
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded" 
+    }
+    data = {"grant_type": "client_credentials"}
+    r = post(url, headers=headers, data=data)
+    json_result = json.loads(r.content)
+    token = json_result["access_token"]
+    return token
+
+def instantiate_app():
+    #Load environment variables
     ENV_FILE = find_dotenv()
     if ENV_FILE:
         load_dotenv(ENV_FILE)
@@ -19,8 +40,8 @@ def instantiate_app():
     app = Flask(__name__)
     app.config.from_object("config.Config")
 
+    #Setup OAuth
     oauth = OAuth(app)
-
     oauth.register(
         "auth0",
         client_id=env.get("AUTH0_CLIENT_ID"),
@@ -31,6 +52,15 @@ def instantiate_app():
         server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration',
     )
 
+    #Setup Spotify API
+    spodify_client_id = env.get("SPOTIFY_CLIENT_ID")
+    spodify_client_secret = env.get("SPOTIFY_CLIENT_SECRET")
+    token = get_token(spodify_client_id, spodify_client_secret)
+    env["SPOTIFY_ACCESS_TOKEN"] = token
+    print(f"TOKEN {env.get('SPOTIFY_ACCESS_TOKEN')}")
+    
+
+    #Setup routes
     @app.route("/callback", methods=["GET", "POST"])
     def callback():
         token = oauth.auth0.authorize_access_token()
--- projectFiles/home/home_service.py
+++ projectFiles/home/home_service.py
@@ -3,13 +3,15 @@ from googlesearch import search
 import re
 import time
 from multiprocessing import Process, Queue
+from os import environ as env
+import json
 
 
-"""
 
-USE JINJA/FLASK INSTEAD OF TKINTER, MORE FUNCTIONAL
 
-"""
+
+
+
 
 class SongNotFoundError(Exception):
     def __init__(self, qry):
@@ -25,20 +27,42 @@ def remove_html_tags(text):
     clean = re.compile('<.*?>')
     return re.sub(clean, '', text)
     
+def get_auth_header(token):
+     return {"Authorization": "Bearer " + token}
 
 
-
-def search_and_open_video(qry, queue):
+def search_and_open_video(qry):
     yt_qry = qry + " youtube"
     for i in search(yt_qry, num=1, stop=1):
             # webbrowser.open(i)
             id = i.split("=")[1]
             url = f"https://www.youtube.com/embed/{id}?autoplay=1&mute=1"
-            print(url)
-            queue.put([url])
+            return url
 
+# def search_spotify(token, qry):
+#     url = "https://api.spotify.com/v1/search"
+#     headers = get_auth_header(token)
+#     spotify_qry = f"?q={qry}&type=track&limit=1"      #Search for track, grab first one
+#     qry_rul = url + spotify_qry
+#     r = requests.get(qry_rul, headers=headers)
+#     json_result = json.loads(r.content)["tracks"]["items"]
+#     #Grab the id of the track
+#     try:
+#         if len(json_result) == 0:
+#              raise SongNotFoundError(qry)
+#         return json_result[0]
+        
+#     except Exception as e:
+#         return None
 
-def search_and_display_key(qry, queue):
+def key_search(qry, queue):
+    # Spotify API deprecated feature
+    # url = f"https://api.spotify.com/v1/tracks/{id}"
+    # headers = get_auth_header(token)
+    # r = requests.get(url, headers=headers)
+    # print(r)
+         
+
     google_qry = qry + " songbpm key"
     for i in search(google_qry, num=1, stop=1):
             r = requests.get(i)
@@ -46,29 +70,33 @@ def search_and_display_key(qry, queue):
             key = re.search('with a ([A-G]) key and a (.*) mode', text)
             try:
                 if key is None:
-                     raise SongNotFoundError
-                queue.put([f"{key.group(1)} {key.group(2)}"])
+                     raise SongNotFoundError(qry)
+                print("Found KEY")
+                queue.put(f"{key.group(1)} {key.group(2)}")
             except Exception as e:
-                 queue.put(["NotFound"])
-    # requests.get()      ####IN THE MIDDLE OF SETTING UP SPOTIFY API
-    # pass
+                 return "NotFound"
+    
+    
                  
 
 def induce_search(qry):
     start = time.time()
-    
+
     q = Queue()
-    p1 = Process(target=search_and_open_video, args=(qry, q))
-    
-    p2 = Process(target=search_and_display_key, args=(qry, q))
-    p1.start()
-    p2.start()
-    p2.join()
-    url = q.get()[0]
-    p1.join()
-    key = q.get()[0]
-    print(f"key {key}, url {url}")
+    p = Process(target=key_search, args=(qry,q))
+    p.start()
+    p.join(timeout=10)
+    if p.is_alive():
+        p.terminate()
+
+    key = "NotFound"
+    if not q.empty() or q.qsize() > 0:
+        key = q.get()
+        
+    q.close()
+
+    url = search_and_open_video(qry)
     print(f"Time taken: {time.time() - start} sec")
 
-
+    
     return key, url
